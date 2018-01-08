85a3e636af6b0160fab242bb38276767e6b7f84b
Thiago Morais <tf_lima@terra.com.br>
2018-01-07 23:47:00 -0200

    Usuário edita uma receita

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,4 +29,23 @@ class RecipesController < ApplicationController
 
   end
 
+  def edit
+    id = params[:id]
+    @recipe = Recipe.find(id)
+    @cuisines = Cuisine.all
+    @recipe_type = RecipeType.all
+  end
+
+  def update
+    id = params[:id]
+    @recipe = Recipe.find(id)
+    recipe_params = params.require(:recipe).permit(:title, :recipe_type_id,
+       :cuisine_id, :difficulty, :cook_time, :people_serve, :ingredients,:method)
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe.id)
+    else
+      redirect_to new_recipe_path, alert: 'Você deve informar todos os dados da receita'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
    root to: 'home#index'
-   resources :recipes, only: [:index, :show, :new, :create]
+   resources :recipes, only: [:index, :show, :new, :create, :edit, :update]
    resources :cuisines, only: [:index, :show, :new, :create]
    resources :recipe_types, only: [:index, :show, :new, :create]
 end
