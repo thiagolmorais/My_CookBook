a1ab57244b76a6944dbe1cccb2cfcc6c45cb8e93
Thiago Morais <tf_lima@terra.com.br>
2018-01-13 00:21:18 -0200

    adição de autenticação (completo)

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -18,4 +18,26 @@ feature 'User login' do
     expect(page).not_to have_link('Entrar')
 
   end
+
+  scenario 'sign out' do
+    user = User.create(email: 'tf_lima@terra.com.br', password: '123456789')
+
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+
+    within('div.actions') do
+      click_on 'Entrar'
+    end
+
+    visit root_path
+    click_on 'Sair'
+
+    expect(page).to have_link('Entrar')
+
+  end
+
+
 end
